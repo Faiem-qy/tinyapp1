abad1a04d848e31954ea81cddcabc92f0aab1cec
Faiem-qy <f.quanyoung@gmail.com>
2023-08-26 12:48:41 -0400

    add cookie session to replace cookie parser, to encryt cookies

--- express_server.js
+++ express_server.js
@@ -1,17 +1,25 @@
-const{generateRandomString, checkIdExists, checkUserId, getUserByEmail} = require('./helpers')
+const { generateRandomString, checkIdExists, checkUserId, getUserByEmail } = require('./helpers');
 
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
-const cookieParser = require('cookie-parser');
-const bcrypt = require("bcryptjs")
+// const cookieParser = require('cookie-parser');
+var cookieSession = require('cookie-session');
+const bcrypt = require("bcryptjs");
 
 app.set("view engine", "ejs");
 
 app.use(express.urlencoded({ extended: true }));
 
 //1A. DISPLAYING USERNAME WITH COOKIE-PARSER
-app.use(cookieParser());
+// app.use(cookieParser());
+app.use(cookieSession({
+  name: 'session',
+  keys: ['key1', 'key2'],
+
+  // Cookie Options
+  maxAge: 24 * 60 * 60 * 1000 // 24 hours
+}));
 
 // Define the initial database of shortened URLs
 const urlDatabase = {
@@ -60,7 +68,7 @@ app.get("/u/:id", (req, res) => {
 
 // Route to render the form for creating new URLs
 app.get("/urls/new", (req, res) => {
-  const user_id = req.cookies.user_id;
+  const user_id = req.session.user_id;
   const templateVars = {
     user_id, //1C. DISPLAYING USERNAME WITH COOKIE-PARSER //4C. we're no longer going to set a username cookie; instead, we will set only a user_id cookie
     user: users[user_id] //4D. Passing the user Object to the _header
@@ -76,7 +84,7 @@ app.get("/urls/new", (req, res) => {
 // Route to display a specific short URL's details
 app.get("/urls/:id", (req, res) => {
   const id = req.params.id;
-  const user_id = req.cookies.user_id;
+  const user_id = req.session.user_id;
   // const longURL = urlDatabase[id].longURL;
   const urlsForUser = checkUserId(user_id, urlDatabase);
   if (!user_id) {//if user is not logged in, prompt login
@@ -106,7 +114,7 @@ app.get("/urls/:id", (req, res) => {
 
 // Route to display a list of all short URLs
 app.get("/urls", (req, res) => {
-  const user_id = req.cookies.user_id;
+  const user_id = req.session.user_id;
   const templateVars = {
     user_id, //1B. DISPLAYING USERNAME WITH COOKIE-PARSER //4C. we're no longer going to set a username cookie; instead, we will set only a user_id cookie
     urls: checkUserId(user_id, urlDatabase),//compare cookie to user
@@ -152,7 +160,7 @@ app.get("/fetch", (req, res) => {
 
 //3A. USER REGISTRATION FORM
 app.get("/register", (req, res) => {
-  const user_id = req.cookies.user_id;
+  const user_id = req.session.user_id;
   const templateVars = {
     user_id, //4C. we're no longer going to set a username cookie; instead, we will set only a user_id cookie
     urls: urlDatabase,
@@ -171,7 +179,7 @@ app.listen(PORT, () => {
 
 //6C . Add a GET route /login that renders the appropriate template
 app.get("/login", (req, res) => {
-  const user_id = req.cookies.user_id;
+  const user_id = req.session.user_id;
   const templateVars = {
     user_id,
     user: users[user_id]
@@ -187,11 +195,11 @@ app.get("/login", (req, res) => {
 app.post("/urls", (req, res) => {
   const id = generateRandomString();// generate random string
   const longURL = req.body.longURL;//
-  const user_id = req.cookies.user_id;
-  console.log(id, longURL); // Log the POST request body to the console
-  urlDatabase[id].longURL = longURL;// add random id to the new longUrl
+  const user_id = req.session.user_id;
+  console.log(id, longURL, urlDatabase[id]); // Log the POST request body to the console
+  urlDatabase[id] = { "longURL": longURL, "userID": user_id };
   console.log(urlDatabase);
-
+  
   if (!user_id) {
     return res.send("You need to register to create URL's"); //If the user is not logged in, POST /urls should respond with an HTML message that tells the user why they cannot shorten URLs
   } else {
@@ -201,8 +209,8 @@ app.post("/urls", (req, res) => {
 
 app.post("/urls/:id/delete", (req, res) => {
   const id = req.params.id;
-  const user_id = req.cookies.user_id;
-  const urlsForUser = checkUserId(user_id,urlDatabase);
+  const user_id = req.session.user_id;
+  const urlsForUser = checkUserId(user_id, urlDatabase);
   if (!user_id) {// if user is not logged in then prompt the user to login
     res.send(`
     <html>
@@ -238,7 +246,7 @@ app.post("/login", (req, res) => {
   const email = req.body.email;
   const password = bcrypt.hashSync(req.body.password, 10);//the user enters this password when logging in which is hashed 
   const user = getUserByEmail(email, users);// this uses the email provided to filter through the users obj and return the specific user
-  console.log(user)
+  console.log(user);
 
   if (email === '' || password === '') { //If empty strings, send back a response with the 400 status code
     return res.status(400).send("Error 400 -To Login Please provide valid email and/or password");
@@ -247,14 +255,14 @@ app.post("/login", (req, res) => {
   } else if (bcrypt.compareSync(password, user.password)) {// this compres the password provided by the user to the stored password in the users object
     return res.status(403).send("Error 403 - Please check your login Information and try again");
   } else { //If email exists, set cookie to the user_id
-    res.cookie('user_id', user.id);
+    req.session.user_id = user.id;
   }
   res.redirect("/urls");
 });
 
 //2A. LOGOUT AND CLEAR COOKIES
 app.post("/logout", (req, res) => {
-  res.clearCookie('user_id'); //4C. we're no longer going to set a username cookie; instead, we will set only a user_id cookie
+  req.session = null; //4C. we're no longer going to set a username cookie; instead, we will set only a user_id cookie
   res.redirect("/login"); //6B. Update the /logout reroute to send the user to our /login page.
 });
 
@@ -271,11 +279,10 @@ app.post("/register", (req, res) => {
     return res.status(400).send("Error 400 - Email already exists");
   } else {
     users[id] = { id, email, password };
-    res.cookie('user_id', users[id].id);
-
+    req.session.user_id = users[id].id;
   }
-  users[id] = { id, email, password };
-  res.cookie('user_id', users[id].id);  //4B. After adding the user, set a user_id cookie containing  //4C. we're no longer going to set a username cookie; instead, we will set only a user_id cookiethe user's newly generated ID.
+  // users[id] = { id, email, password };
+  // req.session.user_id = users[id].id;  //4B. After adding the user, set a user_id cookie containing  //4C. we're no longer going to set a username cookie; instead, we will set only a user_id cookiethe user's newly generated ID.
   console.log(users);
   res.redirect("/urls");
 });
